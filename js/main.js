5961a9070ffd7d7dda887a7b301670531f1880eb
Venrup <venrup@Venrups-MacBook-Pro.local>
2022-10-20 18:24:33 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
