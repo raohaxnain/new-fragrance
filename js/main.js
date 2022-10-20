ba817697a609c3ba3017574911a968df99bf53aa
Rao hasnain <85988579+raohaxnain@users.noreply.github.com>
2022-10-20 19:56:01 +0500

    Updated fragrance

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 16,
+    breakpoints: {
+        375: {
+            slidesPerView: 1.5,
+            spaceBetween: 16,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+        },
+        // when window width is >= 640px
+        1920: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+        }
+      },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
